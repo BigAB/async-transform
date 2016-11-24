904b95f0c906904c2fbde7f701a76a273dc5072d
Adam Barrett <adamlbarrett@hotmail.com>
2016-11-23 22:00:55 -0600

    1.0.3

---
+++ dist/cjs/async-transform.js
@@ -0,0 +1,22 @@
+/*@bigab/async-transform@1.0.2#async-transform*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+function asyncTransform(transforms, val, context) {
+    if (typeof val === 'undefined') {
+        return function (v) {
+            return asyncTransform(transforms, v, context);
+        };
+    }
+    var v = Promise.resolve(val);
+    if (context) {
+        transforms = transforms.map(function (t) {
+            return function (v) {
+                return t.call(context, v);
+            };
+        });
+    }
+    return transforms.reduce(function (p, t) {
+        return p.then(t);
+    }, v);
+}
+exports.default = asyncTransform;
